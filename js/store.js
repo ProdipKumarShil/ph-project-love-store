3da0b3fa8ac2720ea3223f0ccac4e2dac7fadd0b
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-02-14 18:09:57 +0600

    active quantity btn

--- js/store.js
+++ js/store.js
@@ -1,3 +1,7 @@
 deleteCard('roseDismiss')
 deleteCard('chocolateDismiss')
 deleteCard('dairyDismiss')
+
+plusMinusQuantity("dairyQuantity", "dairyPlusBtn", "dairyMinusBtn")
+plusMinusQuantity("roseQuantity", "rosePlusBtn", "roseMinusBtn")
+plusMinusQuantity("chocolateQuantity", "chocolatePlusBtn", "chocolateMinusBtn")
--- js/utilities.js
+++ js/utilities.js
@@ -3,3 +3,14 @@ function deleteCard(btnId){
         event.target.parentNode.parentNode.parentNode.style.display = 'none';
     })
 }
+function plusMinusQuantity(field, btn, btn2){
+    let count = 0;
+    document.getElementById(btn).addEventListener('click', function(){
+        document.getElementById(field).value = count += 1;
+    })
+    document.getElementById(btn2).addEventListener('click', function(){
+        document.getElementById(field).value = count -= 1;
+    })
+}
+
+
