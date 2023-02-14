d42f58872b98e69cd65dc70a5663205a2f1f9369
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-02-14 20:10:40 +0600

    added pomo code validation

--- js/store.js
+++ js/store.js
@@ -9,3 +9,15 @@ plusMinusQuantity("chocolateQuantity", "chocolatePlusBtn", "chocolateMinusBtn")
 setValue("chocolateBuyBtn", "chocolateQuantity", 200)
 setValue("roseBuyBtn", "roseQuantity", 100)
 setValue("dairyBuyBtn", "dairyQuantity", 100)
+
+document.getElementById('pomoBtn').addEventListener('click', function(event){
+    const code = document.getElementById('pomoInput').value;
+    if(code == 102){
+        const totalCostAmount = parseInt(document.getElementById('totalCost').innerText);
+        document.getElementById('totalCost').innerText = totalCostAmount - (totalCostAmount * 0.2);
+    }
+    else{
+        alert("Wrong Pomo Code");
+    }
+})
+
