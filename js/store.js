c13b5f1e771da30a9dad6cce777eb41af0ed30f8
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-02-14 19:52:19 +0600

    added calculating system

--- js/store.js
+++ js/store.js
@@ -5,3 +5,7 @@ deleteCard('dairyDismiss')
 plusMinusQuantity("dairyQuantity", "dairyPlusBtn", "dairyMinusBtn")
 plusMinusQuantity("roseQuantity", "rosePlusBtn", "roseMinusBtn")
 plusMinusQuantity("chocolateQuantity", "chocolatePlusBtn", "chocolateMinusBtn")
+
+setValue("chocolateBuyBtn", "chocolateQuantity", 200)
+setValue("roseBuyBtn", "roseQuantity", 100)
+setValue("dairyBuyBtn", "dairyQuantity", 100)
--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,7 @@
+document.getElementById("dairyQuantity").value = 0;
+document.getElementById("roseQuantity").value = 0;
+document.getElementById("chocolateQuantity").value = 0;
+
 function deleteCard(btnId){
     document.getElementById(btnId).addEventListener('click', function(event){
         event.target.parentNode.parentNode.parentNode.style.display = 'none';
@@ -12,5 +16,18 @@ function plusMinusQuantity(field, btn, btn2){
         document.getElementById(field).value = count -= 1;
     })
 }
+
+const totalCost = document.getElementById('totalCost');
+const totalBalanceField = document.getElementById('totalBalance');
+
+function setValue(buyBtn, inputField, price) {
+    document.getElementById(buyBtn).addEventListener('click', function () {
+        const quantityNum = parseInt(document.getElementById(inputField).value)
+        totalCost.innerText = parseInt(totalCost.innerText) + (quantityNum * price);
+        totalBalanceField.innerText = parseInt(totalBalanceField.innerText) - (quantityNum * price);
+        document.getElementById(inputField).value = ''
+    })
+}
+
 
 
